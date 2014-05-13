3408b1dfc2f9a3aa0d6463415d81b52b06d5ee35
Brian Ford <btford@umich.edu>
2014-05-13 16:37:01 -0700

    chore: fix whitespace in example

--- examples/app.js
+++ examples/app.js
@@ -3,16 +3,16 @@ import {ComponentDirective} from 'templating';
 import {RootRouter} from 'router';
 
 @ComponentDirective({
-	selector:'ng-app'
+  selector:'ng-app'
 })
 export class App {
   @Inject(RootRouter)
   constructor(router) {
-  	this.router = router;
+    this.router = router;
 
-  	router.title = 'Router Demo';
+    router.title = 'Router Demo';
     router = router.map([
-    	{ route: ['','welcome'], title:'Welcome', moduleId: 'welcome', nav: true },
+      { route: ['','welcome'], title:'Welcome', moduleId: 'welcome', nav: true },
       { route: 'flickr', moduleId: 'flickr', nav: true }
     ]);
   }
