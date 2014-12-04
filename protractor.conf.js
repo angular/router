c0994cd89d1a8658f5f1c3f49ac438b76d8fdc3a
Brian Ford <btford@umich.edu>
2014-12-04 15:55:02 -0800

    chore(protractor): add config

---
+++ protractor.conf.js
@@ -0,0 +1,19 @@
+exports.config = {
+  allScriptsTimeout: 11000,
+
+  specs: [
+    'examples/**/scenario.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
